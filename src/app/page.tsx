18a20016d4abbcb3d11c0ae3d1fdc4880c61b268
AmeenCodes01 <masaqib5@gmail.com>
2025-12-13 12:52:19 +0000

    06:background-jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,32 @@
 
+"use client"
 import { Button } from "@/components/ui/button"
 import { requireAuth } from "@/lib/auth-utils"
-import { caller } from "@/trpc/server"
 import SignOut from "../features/auth/components/SignOut"
+import { useTRPC } from "@/trpc/client"
+import { QueryClient, useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { toast } from "sonner"
 
-const Page = async()=>{
-await requireAuth()
-const data = await caller.getUsers()
+const Page = ()=>{
+//await requireAuth()
+const trpc = useTRPC()
+const {data} =useQuery(trpc.getWorkflows.queryOptions())
+const queryClient = useQueryClient()
+const create =useMutation(trpc.createWorkflow.mutationOptions(
+  {
+    onSuccess: ()=>{
+  toast.success("Job queued")  
+    }
+  }
+))
+
   return (
     <div className="text-red-200">
       {JSON.stringify(data)}
       <SignOut/>
+      <Button disabled={create.isPending} onClick={()=> create.mutate()}>
+        Create workflow
+      </Button>
       </div>
   )
 }
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,15 +1,21 @@
-import { z } from 'zod';
-import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
+import { email, z } from 'zod';
+import {  createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
+import { inngest } from '@/inngest/client';
 export const appRouter = createTRPCRouter({
- getUsers: protectedProcedure.query(({ctx})=>{
-return prisma.user.findMany({
-    where:{
-            id:ctx.auth.user.id
- }
-  
+    getWorkflows: protectedProcedure.query(({ ctx }) => {
+        console.log(process.env.DATABASE_URL)
+        return prisma.workflow.findMany()
+    }),
+    createWorkflow: protectedProcedure.mutation(async()=>{
+        await inngest.send({
+            name:"test/hello.world",
+            data:{
+                email:"masaqib5@gmail.com" 
+            }
+        })
+        return {success: true, message:"Job queued"}
     })
-})
 
 });
 // export type definition of API
